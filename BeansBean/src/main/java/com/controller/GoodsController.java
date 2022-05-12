04a7f455ec0cf1f17a63c67cd0558a6c25668503
정수현 <sue5316@naver.com>
2022-05-12 14:43:19 +0900

    수정

---
+++ BeansBean/src/main/java/com/controller/GoodsController.java
@@ -0,0 +1,57 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dto.BundleDTO;
+import com.dto.GoodsDTO;
+import com.service.GoodsService;
+
+@Controller
+public class GoodsController {
+	
+	@Autowired
+	GoodsService service;
+	
+	
+	
+	@RequestMapping("/goodsList")
+	public ModelAndView goodsList(String gcategory) {
+		// TODO 상품목록보기
+//		System.out.println(gcategory);
+		if(gcategory == null) {
+			gcategory = "coffee";
+		}
+
+		List<GoodsDTO> list = service.goodsList(gcategory);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("goodsList", list);
+//		System.out.println(list);
+		mav.setViewName("main");
+		return mav;
+	}
+	
+	@RequestMapping("/goodsDetail")
+	@ModelAttribute("goodsDetail")
+	public GoodsDTO goodsDetail(String gcode) {
+//		System.out.println(gcode);
+		GoodsDTO dto = service.goodsDetail(gcode);
+//		System.out.println(dto);
+		return dto;
+	}
+	
+//	//@RequestMapping("/goodsDetail")
+//	public BundleDTO goodsBundle(String gcode){
+//		BundleDTO dto = new BundleDTO();
+//		System.out.println(gcode);
+//		return dto;
+//	}
+	
+}
